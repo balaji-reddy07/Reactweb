e0f23173e75a544114d6a189639fe94f9440a153
Balaji Reddy <101589747+balaji-reddy07@users.noreply.github.com>
2023-07-27 13:34:44 +0530

    Add files via upload

---
+++ schemas/index.js
@@ -0,0 +1,11 @@
+import testimonial from "./testimonial"
+import works from './works'
+import brands from './brands'
+import abouts from './abouts'
+import experiences from './experiences'
+import skills from './skills'
+import workExperience from './workExperience'
+import contact from './contact'
+
+
+export const schemaTypes = [works, testimonial, brands, abouts, skills, workExperience, experiences, contact]
